4a49e46438e7f79b4119feeecc1def58182cd942
tanujagar621 <tanujagarwal621@gmail.com>
2021-05-15 21:32:39 +0530

    Edit UserName added

--- contact.php
+++ contact.php
@@ -9,16 +9,39 @@
     <!-- Bootstrap CSS -->
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title>Contact iCode</title>
 </head>
 
 <body>
     <?php require "components/_nav.php";?>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+    </button>
 
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php require "components/_footer.php";?>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
@@ -27,6 +50,9 @@
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
     </script>
 </body>
 
--- selfProfile.php
+++ selfProfile.php
@@ -7,7 +7,7 @@ function test_input($data) {
     $data = htmlspecialchars($data);
     return $data;
 }
-    
+
 ?>
 
 <!doctype html>
@@ -24,10 +24,81 @@ function test_input($data) {
 
 <body>
     <?php require "components/_nav.php";?>
+    <?php
+    $id = $_SESSION['id'];
+    $showAlert = false;
+    $showErr = false;
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $username = $_POST['username'];
+        $sql = "select * from `users` where `User_email` = '$username'";
+        $result = mysqli_query($con, $sql);
+        $num = mysqli_num_rows($result);
+        if($num == 0)
+        {
+            $sql = "update `users` set `User_email` = '$username' where `User_id` = '$id'";
+            $result = mysqli_query($con, $sql);
+            if($result)
+            {
+                $showAlert = true;
+            }
+        }
+        else{
+            $showErr = "UserName Already Exists";
+        }
+    }
+
+    if($showErr)
+    {
+        echo '
+        <div class="alert alert-danger alert-dismissible fade show my-0" role="alert">
+            <strong>Error!</strong>'.$showErr.'
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>';
+    }
+    if($showAlert)
+    {
+        echo '
+        <div class="alert alert-success alert-dismissible fade show my-0" role="alert">
+            <strong>Success! </strong> your username has been updated successfully.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>';
+    }
+    ?>
+    <?php
+        echo "
+            <!-- Modal -->
+            <div class='modal fade' id='editModal' tabindex='-1' aria-labelledby='editModalLabel' aria-hidden='true'>
+            <div class='modal-dialog'>
+                <div class='modal-content'>
+                <div class='modal-header'>
+                    <h5 class='modal-title' id='editModalLabel'>Modal title</h5>
+                    <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                </div>
+                <div class='modal-body'>
+                    <form action='selfProfile.php' method='post'>
+                        <div class='mb-3'>
+                            <label for='username' class='form-label'>Enter New UserName</label>
+                            <input type='text' class='form-control' name='username' id='username' aria-describedby='emailHelp'>
+                        </div>
+                        <button type='submit' class='btn btn-primary'>Submit</button>
+                    </form>
+                </div>
+                <div class='modal-footer'>
+                    <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
+                    <button type='button' class='btn btn-primary'>Save changes</button>
+                </div>
+                </div>
+            </div>
+            </div>"
+    ?>
     <div style="min-height: 85vh;" class="container">
         <div class="username">
             <?php
-            $id = $_SESSION['id'];
             $sql = "select * from `users` where `User_id` = '$id'";
             $result = mysqli_query($con, $sql);
             $row = mysqli_fetch_assoc($result);
@@ -37,7 +108,7 @@ function test_input($data) {
                 <p class="lead"></p>
                 <hr class="my-4">
                 <p></p>
-                <a class="btn btn-success btn-lg" href="#" role="button">Edit Details</a>
+                <a class="btn btn-success btn-lg" href="#" data-bs-toggle="modal" data-bs-target="#editModal" role="button">Edit Details</a>
                 <a class="btn btn-success btn-lg" href="components/_logout.php" role="button">LogOut</a>
                 </div>
                 <div class="questions">
@@ -120,6 +191,9 @@ function test_input($data) {
         </script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
             integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
         </script>
 </body>
 
